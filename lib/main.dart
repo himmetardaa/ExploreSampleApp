3340372dd46ea614fa69ff5ff203b9c20114edc1
Himmet Arda <sekizarda4343@hotmail.com>
2023-11-16 23:14:57 +0300

    Backup

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,18 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Header(),
-            Expanded(child: Container(width: double.infinity)),
+            Expanded(
+                child: Container(
+              width: double.infinity,
+              child: Column(children: [
+                SearchBox(),
+                Divider(),
+                TopLocation(),
+                Divider(),
+                NearLocation(),
+                Divider(),
+              ]),
+            )),
             BottomMenu(),
           ],
         ),
@@ -32,7 +43,150 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Widget Header() => Container(width: double.infinity, height: 80);
+  Widget NearLocation() => Container(
+        child: Column(
+          children: [TitleItem("Near You", "View All")],
+        ),
+      );
+
+  Widget TopLocation() => Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TitleItem("Top Locations", "View All"),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  StoryItem("assets/images/foto.jpg", "Arda A."),
+                  StoryItem("assets/images/man.png", "Ahmet T."),
+                  StoryItem("assets/images/girl.png", "İrem S."),
+                  StoryItem("assets/images/foto.jpg", "Arda A."),
+                  StoryItem("assets/images/man.png", "Ahmet T."),
+                  StoryItem("assets/images/girl.png", "İrem S."),
+                  StoryItem("assets/images/girl.png", "İrem S.")
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget SearchBox() => Container();
+
+  Widget StoryItem(String photo, String name) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(3),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 241, 156, 101),
+                  Color.fromARGB(255, 233, 92, 70),
+                ]),
+                shape: BoxShape.circle),
+            child: Container(
+              padding: EdgeInsets.all(2),
+              decoration:
+                  BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+              child: CircleAvatar(
+                backgroundImage: AssetImage(photo),
+                radius: 35,
+              ),
+            ),
+          ),
+          Text(
+            name,
+            style: TextStyle(fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding TitleItem(String title, String link) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Color.fromARGB(255, 56, 56, 56),
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            link,
+            style: TextStyle(
+              color: Color.fromARGB(255, 153, 153, 153),
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget Header() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          //color: Color.fromARGB(255, 243, 243, 243),
+          height: 80,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello Welcome!",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 153, 153, 153),
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    "Explore City",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 56, 56, 56),
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white, shape: BoxShape.circle),
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.sunny,
+                      color: Color.fromRGBO(236, 125, 87, 1),
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white, shape: BoxShape.circle),
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.notifications),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
 
   Widget BottomMenu() => Container(
         width: double.infinity,
@@ -47,7 +201,7 @@ class MyApp extends StatelessWidget {
           children: [
             BottomMenuItem("Home", Icons.home, true),
             BottomMenuItem("Moments", Icons.groups, false),
-            BottomMenuItem("Book", Icons.maps_ugc, false),
+            HighlightedItem("Book", Icons.add_box),
             BottomMenuItem("Chat", Icons.chat, false),
             BottomMenuItem("Profile", Icons.person, false),
           ],
@@ -74,6 +228,32 @@ class MyApp extends StatelessWidget {
           ),
           SizedBox(
             height: 3,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 10,
+              color: renk,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget HighlightedItem(String title, IconData icon) {
+    var renk = Color.fromRGBO(236, 125, 87, 1);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(
+            icon,
+            size: 38,
+            color: renk,
           ),
           Text(
             title,
